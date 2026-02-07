3192d348dc12d353ba8cb182cba0c18f9383027b
Error000000000 <xoxoerror00@gmail.com>
2026-02-07 21:09:38 +0600

    Rename ContactMail to PortfolioContactMail to fix class not found error

---
+++ app/Mail/PortfolioContactMail.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+/**
+ * Class PortfolioContactMail
+ * 
+ * Mailable for sending contact form submissions.
+ * 
+ * @package App\Mail
+ */
+class PortfolioContactMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * @var array
+     */
+    public array $data;
+
+    /**
+     * Create a new message instance.
+     * 
+     * @param array $data
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'New Contact Message from Portfolio - ' . $this->data['name'],
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.contact',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
